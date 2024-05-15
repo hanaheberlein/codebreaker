98d0f92d9e782a5e4c35a83e5e974bf51228f8d3
hanaheberlein <hanaheberlein@gmail.com>
2024-05-15 11:53:11 -0400

    First Commit

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" class="h-full">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <title>Codebreaker</title>
+</head>
+<body class="pt-8 bg-primary text-white print:bg-white print:text-black grid place-items-center h-full">
+    {{ $slot }}
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<x-layout>
+
+<section class="container mx-aut0 px-6 h-full flex flex-col" style="max-width: 1000px;">
+        <header class="flex justify-start">
+            <h1>
+                <img src="/logo.svg" alt="Codebreaker" aria-label="codebreaker" class="print:invert w-52 print:w-36">
+            </h1>
+        </header>
+
+    <livewire:create-code />
+</section>
+
+</x-layout>
